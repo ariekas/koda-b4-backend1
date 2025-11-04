cebf51fce48ca743614544da66bc71ae13a291ef
ariekas <ariariekas@gmail.com>
2025-11-04 16:51:59 +0700

    feat: creating api deleted user

--- main.go
+++ main.go
@@ -20,10 +20,10 @@ type Response struct {
 }
 
 var users = []User{
-	{Id: 1, Name: "ari eka", Email: "ari@gmail.com", Password: "1234"},
-	{Id: 2, Name: "yanto", Email: "ynt@gmail.com", Password: "12"},
-	{Id: 3, Name: "paw paw", Email: "pkw@gmail.com", Password: "ppack"},
-	{Id: 4, Name: "knakri", Email: "aw@gmail.com", Password: "aw123"},
+	// {Id: 1, Name: "ari eka", Email: "ari@gmail.com", Password: "1234"},
+	// {Id: 2, Name: "yanto", Email: "ynt@gmail.com", Password: "12"},
+	// {Id: 3, Name: "paw paw", Email: "pkw@gmail.com", Password: "ppack"},
+	// {Id: 4, Name: "knakri", Email: "aw@gmail.com", Password: "aw123"},
 }
 
 func main() {
@@ -74,6 +74,26 @@ func main() {
 			Success: true,
 			Message: "Success Create User!",
 			Data:    []User{newuser},
+		})
+	})
+
+	r.DELETE("/users/:id", func(ctx *gin.Context) {
+		id := ctx.Param("id")
+		for i, user := range users{
+			if fmt.Sprint(user.Id) == id {
+				users = append(users[:i], users[i+1:]...)
+
+				ctx.JSON(200, Response{
+					Success: true,
+					Message: "Deleting data!",
+				})
+				return
+			}
+		}
+
+		ctx.JSON(404, Response{
+			Success: false,
+			Message: "User not found",
 		})
 	})
 
